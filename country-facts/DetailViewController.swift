e2cc06a4454c64f4c9fd1f4e375a3cd415e96c7a
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-03-31 11:42:24 +0100

    Forget to set initial view controller

---
+++ country-facts/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  country-facts
+//
+//  Created by Bradley Chesworth on 31/03/2020.
+//  Copyright © 2020 Brad Chesworth. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
