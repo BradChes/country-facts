3fb68224e36556d165863d941ade81a4fe5ba201
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-03-31 09:55:15 +0100

    init project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Country Facts
+<p align="center">
+  <img src="demo/app-icon.png " alt="Country Facts application icon"
+	  title="Country Facts application icon" align="center" width="200" height="200" />
+</p>
+</br>
+An application that contains facts about countries
+
+## Examples within this code
+To be added.
+
+## Demonstration
+To be added.
---
+++ country-facts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  country-facts
+//
+//  Created by Bradley Chesworth on 31/03/2020.
+//  Copyright © 2020 Brad Chesworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
